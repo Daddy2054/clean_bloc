80188a929e83ea75c7244468dbdb6c4a32833832
Jean <acalto2@gmail.com>
2023-11-12 23:48:37 +0100

    init and 1.9: App Theme

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// ignore_for_file: public_member_api_docs
+
+import 'package:clean_bloc/src/config/app_theme.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: CustomTheme().theme(),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Flutter App with Clean Architecture')),
+    );
+  }
+}
