11d040980cff4b7a64625a07b34e5e8787883696
Jean <acalto2@gmail.com>
2023-11-14 20:36:35 +0100

    2.8: Auth Use Cases

---
+++ lib/src/features/auth/domain/usecases/login_user.dart
@@ -0,0 +1,31 @@
+import '../../../../shared/domain/entities/user.dart';
+import '../../../../shared/domain/usecases/usecases.dart';
+import '../entities/logged_in_user.dart';
+import '../repositories/auth_repository.dart';
+
+class LoginUser implements UseCase<void, LoginUserParams> {
+  final AuthRepository authRepository;
+
+  LoginUser(this.authRepository);
+
+  @override
+  Future<void> call(LoginUserParams params) {
+    return authRepository.login(
+      username: params.username,
+      password: params.password,
+    );
+  }
+}
+
+class LoginUserParams extends Params {
+  final Username username;
+  final Password password;
+
+  LoginUserParams({
+    required this.username,
+    required this.password,
+  });
+
+  @override
+  List<Object?> get props => [username, password];
+}
