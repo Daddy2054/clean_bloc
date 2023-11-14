4accd6ed07efbdcd0f95c140e629cecad5dc7ff4
Jean <acalto2@gmail.com>
2023-11-14 20:13:29 +0100

    2.7: Auth Repository

---
+++ lib/src/features/auth/data/repositories/auth_repository_impl.dart
@@ -0,0 +1,37 @@
+import '../../../../shared/domain/entities/user.dart';
+import '../../domain/entities/logged_in_user.dart';
+import '../../domain/repositories/auth_repository.dart';
+import '../datasources/mock_auth_datasource.dart';
+
+class AuthRepositoryImpl extends AuthRepository {
+  final MockAuthDatasource authDatasource;
+
+  AuthRepositoryImpl(this.authDatasource);
+
+  @override
+  Stream<AuthStatus> get status => authDatasource.status;
+
+  @override
+  Future<LoggedInUser> get loggedInUser => authDatasource.loggedInUser;
+
+  @override
+  Future<void> login({
+    required Username username,
+    required Password password,
+  }) {
+    return authDatasource.login(
+      username: username,
+      password: password,
+    );
+  }
+
+  @override
+  Future<void> logout() {
+    return authDatasource.logout();
+  }
+
+  @override
+  Future<void> signup({required LoggedInUser loggedInUser}) {
+    return authDatasource.signup(loggedInUser: loggedInUser);
+  }
+}
