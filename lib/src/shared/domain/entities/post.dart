9b9c6fd5e691b63bd3d0d5b20e30dd5b4ac8d84e
Jean <acalto2@gmail.com>
2023-11-15 21:59:03 +0100

    3.2: Post Entity

---
+++ lib/src/shared/domain/entities/post.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+import 'user.dart';
+
+class Post extends Equatable {
+  final String id;
+  final User user;
+  final String caption;
+  final String assetPath;
+
+  const Post({
+    required this.id,
+    required this.user,
+    required this.caption,
+    required this.assetPath,
+  });
+
+  @override
+  List<Object?> get props => [
+        id,
+        user,
+        caption,
+        assetPath,
+      ];
+}
